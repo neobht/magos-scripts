cf7004922f750d7b157cc0f6fde1af4226428dae
Anton Goroshkin <neobht@gmail.com>
2016-03-20 19:08:09 +0300

    some fixes

--- 90-magos-patches/usr/lib/magos/rootfs-patches/MagOS-os_release.sh
+++ 90-magos-patches/usr/lib/magos/rootfs-patches/MagOS-os_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo SYSTEM=\"MagOS Linux\" > etc/sysconfig/oem
-echo PRODUCT=2012 >> etc/sysconfig/oem
+echo PRODUCT=2014 >> etc/sysconfig/oem
 cat >etc/os-release <<EOF
 NAME="MagOS Linux 2014"
 VERSION="2014"
